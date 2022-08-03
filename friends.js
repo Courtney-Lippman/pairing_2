099b810c9534249d1c0f7bd9379205741821bd29
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-03 14:20:03 -0600

    Add places

--- friends.js
+++ friends.js
@@ -5,3 +5,8 @@ console.log(arrays[2])
 console.log(arrays[4])
 
 console.log(arrays[0])
+
+var places = ["Colorado", "Virginia", "Florida", "California", "Texas"]
+
+places.pop()
+console.log(places)
